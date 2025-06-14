4b00a8503c4c6a8cda8eaa69fc5d7600e2a5e910
Joey Salakha <joeysalakha81@gmail.com>
2025-06-14 14:52:17 +0000

    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsby

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9370081174240595"
+     crossOrigin="anonymous"></script>
       </head>
       <body className="font-body antialiased bg-background text-foreground">
         {children}
